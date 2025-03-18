53c79cea6840819e6dc3115448f90e8e367fb463
Shree <shree@gmail.com>
2025-03-18 16:41:39 +0530

    First Commit

---
+++ src/test/java/com/psa/PsaGitExaplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsaGitExaplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
